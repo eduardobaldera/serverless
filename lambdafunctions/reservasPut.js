fcad6e02b55778e24519c0af144307978d7f8caf
eduardobaldera <20170982@ce.pucmm.edu.do>
2020-12-07 13:42:03 -0400

    Initial commit

---
+++ lambdafunctions/reservasPut.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+exports.handler = async(event, context) =>{
+    const documentClient = new AWS.DynamoDB.DocumentClient();
+
+    let responseBody = "";
+    let statusCode = 0;
+
+    const { id, correo, nombre, laboratorio, fecha, horaInicio, horaFinal } = JSON.parse(event.body);
+
+    const params = {
+        TableName: "Reservas",
+        Item: {
+            id: id,
+            correo: correo,
+            nombre: nombre,
+            laboratorio: laboratorio,
+            fecha: fecha,
+            horaInicio: horaInicio,
+            horaFinal: horaFinal
+        }
+    };
+
+    try{
+        const data = await documentClient.put(params).promise();
+        responseBody = JSON.stringify(data);
+        statusCode = 201;
+    }catch (err){
+        responseBody = `Error en registro de reserva: ${err}`;
+        statusCode = 403;
+    }
+
+     const response = {
+        statusCode: statusCode,
+        headers: {
+            "Content-Type": "application/json",
+            //"access-control-allow-origin": "*"
+        },
+        body: responseBody
+    };
+
+    return response;
+};
